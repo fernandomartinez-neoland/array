c7ae8084483a675e9639d62b113eaa417d0a8f27
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-19 11:04:49 +0100

    inicios

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const alumnos = [
+    "jose",
+    "kaleff",
+    "marlon",
+    "marc",
+    "alejo",
+    "julia",
+    "cesar"
+]
+
+const agregar_alumnos = "aaaaaa"
+const agregar_primero = "bbbbb"
+
+const eultimo = document.getElementsById("Eultimo");
+const aultimo = document.getElementsById("Aultimo");
+const eprimero = document.getElementsById("Eprimero");
+const aprimero = document.getElementsById("Aprimero");
+
+
+eultimo.addeEventlistener
