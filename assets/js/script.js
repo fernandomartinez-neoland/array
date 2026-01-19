119cd8155a9d254357100032f9ca116fcd5b35ab
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-19 11:47:19 +0100

    push array

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,29 @@ const alumnos = [
 const agregar_alumnos = "aaaaaa"
 const agregar_primero = "bbbbb"
 
-const eultimo = document.getElementsById("Eultimo");
-const aultimo = document.getElementsById("Aultimo");
-const eprimero = document.getElementsById("Eprimero");
-const aprimero = document.getElementsById("Aprimero");
+const eultimo = document.getElementById("Eultimo");
+const aultimo = document.getElementById("Aultimo");
+const eprimero = document.getElementById("Eprimero");
+const aprimero = document.getElementById("Aprimero");
 
 
-eultimo.addeEventlistener
+eultimo.addEventListener("click", () => {
+    console.log("eultimo")
+})
+
+aultimo.addEventListener("click", () => {
+
+    console.log("antes: ", alumnos)
+    alumnos.push(agregar_alumnos);
+    // alumnos[alumnos.length] = agregar_alumnos;
+    console.log("despues: ", alumnos)
+
+})
+
+eprimero.addEventListener("click", () => {
+    console.log("eprimero")
+})
+
+aprimero.addEventListener("click", () => {
+    console.log("aprimero")
+})
